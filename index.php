e4842050ce1011e8526bee8be77b20c5f1c8e2cd
Berk <68653647+Berk404@users.noreply.github.com>
2022-06-18 14:21:02 -0300

    Few meta-data changes
    Signed-off-by: Berk <68653647+Berk404@users.noreply.github.com>

--- index.php
+++ index.php
@@ -19,11 +19,27 @@
         <meta name="author" content="CloudTeam" />
         <meta name="copyright" content="CloudTeam © <?php echo date("Y"); ?>" />
 <!-- Extra -->
-<meta property="og:image" content="codes.cloudteam.me/assets/img/banner.png" />
+<meta property="og:image" content="https://cloudteam.me/assets/img/banner.png" />
 <!-- =========================== ^ To-Fix ===========================  -->
 <!-- Spiders indexation -->
 <meta name="robots" content= "index, follow">
-<!-- Favicons -->
+        <!-- favicons -->
+<link rel="apple-touch-icon" sizes="57x57" href="https://cloudteam.me/assets/favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="https://cloudteam.me/assets/favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="https://cloudteam.me/assets/favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="https://cloudteam.me/assets/favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="https://cloudteam.me/assets/favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="https://cloudteam.me/assets/favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="https://cloudteam.me/assets/favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="https://cloudteam.me/assets/favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="https://cloudteam.me/assets/favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://cloudteam.me/assets/favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://cloudteam.me/assets/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="https://cloudteam.me/assets/favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://cloudteam.me/assets/favicon/favicon-16x16.png">
+<link rel="manifest" href="https://cloudteam.me/assets/favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#2571e8">
+<meta name="msapplication-TileImage" content="https://cloudteam.me/assets/favicon/ms-icon-144x144.png">
 <!-- =========================== ^ To-Do ===========================  -->
 <meta name="msapplication-TileColor" content="#2571e8">
 <!-- CDN Links -->
